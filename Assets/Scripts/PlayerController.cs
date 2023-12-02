53134a482d6a4d2ee9016e9df89c9a7742f6df27
Hamdi Tarı <hamditari@gmail.com>
2023-12-02 20:42:50 +0300

    4.Ünite tamamlandı.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //hız değişkenini public olarak tanımladığımızda unity editor içerisinden erişebiliriz
-    public float speed = 10.0f;
+    //değişkenleri public olarak tanımladığımızda unity editor içerisinden erişebiliriz
+    private float speed = 10.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +19,17 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        //aracı ileri doğru hareket ettiriyoruz
-        //transform.Translate(0,0,1); ////Her framede 1 metre ilerler
-        //transform.Translate(Vector3.forward); //Her framede 1 metre ilerler (yukarıdaki ile aynı)
-        //transform.Translate(Vector3.forward * Time.deltaTime); //Her saniyede 1 metre ilerler
-        //transform.Translate(Vector3.forward * Time.deltaTime * 20); //Her saniyede 20 metre ilerler
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); //Her saniyede speed metre kadar ilerler
+    {        
+        //Kullanıcı girişlerini (sağ-sol, ileri-geri) alıyoruz
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //İleri-geri hareket
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //Sağa sola dönüş
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput); //sağa sola kayarak gider
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput); //Sağa sola döner
     }
 }
